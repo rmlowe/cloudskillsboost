fd0eed4091fa4f0245c7f663361dfb1ca08bf685
Robert Lowe <robertmlowe@rmlowe.com>
2022-03-30 15:03:35 +0100

    Introduce envvar for BigQuery table name

--- perform-foundational-data-ml-and-ai-tasks-in-google-cloud-challenge-lab/1-dataflow.sh
+++ perform-foundational-data-ml-and-ai-tasks-in-google-cloud-challenge-lab/1-dataflow.sh
@@ -5,4 +5,4 @@ gcloud dataflow jobs run simple-job \
     --gcs-location gs://dataflow-templates-us-central1/latest/GCS_Text_to_BigQuery \
     --region us-central1 \
     --staging-location gs://$CLOUD_STORAGE_BUCKET_NAME/temp \
-    --parameters javascriptTextTransformGcsPath=gs://cloud-training/gsp323/lab.js,JSONPath=gs://cloud-training/gsp323/lab.schema,javascriptTextTransformFunctionName=transform,outputTable=$PROJECT_ID:$BIGQUERY_DATASET_NAME.customers_997,inputFilePattern=gs://cloud-training/gsp323/lab.csv,bigQueryLoadingTemporaryDirectory=gs://$CLOUD_STORAGE_BUCKET_NAME/bigquery_temp
+    --parameters javascriptTextTransformGcsPath=gs://cloud-training/gsp323/lab.js,JSONPath=gs://cloud-training/gsp323/lab.schema,javascriptTextTransformFunctionName=transform,outputTable=$PROJECT_ID:$BIGQUERY_DATASET_NAME.$BIGQUERY_TABLE_NAME,inputFilePattern=gs://cloud-training/gsp323/lab.csv,bigQueryLoadingTemporaryDirectory=gs://$CLOUD_STORAGE_BUCKET_NAME/bigquery_temp
